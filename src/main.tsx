efe1d29832e5c1e64d345144153237412891297d
whdgur5717 <whdgur57117@gmail.com>
2025-04-02 22:49:06 +0900

    monaco-editor worker,typescript 관련 설정 - 언어별 worker 설정, js/ts의 경우 custom 워커 사용 - js/ts compilerOptions 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,51 @@
 import { createRoot } from "react-dom/client"
 import App from "./App.tsx"
+import * as monaco from "monaco-editor"
+import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker"
+import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker"
+import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker"
+import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker"
+
+self.MonacoEnvironment = {
+  getWorker(_, label) {
+    if (label === "json") {
+      return new jsonWorker()
+    }
+    if (label === "css" || label === "scss" || label === "less") {
+      return new cssWorker()
+    }
+    if (label === "html" || label === "handlebars" || label === "razor") {
+      return new htmlWorker()
+    }
+    if (label === "javascript" || label === "typescript") {
+      return new Worker("/worker.js", { type: "module" })
+    }
+    return new editorWorker()
+  },
+}
+
+monaco.languages.typescript.javascriptDefaults.setDiagnosticsOptions({
+  noSemanticValidation: false,
+  noSyntaxValidation: false,
+})
+monaco.languages.typescript.javascriptDefaults.setCompilerOptions({
+  target: monaco.languages.typescript.ScriptTarget.ESNext,
+  allowJs: true,
+  checkJs: true,
+  allowNonTsExtensions: true,
+})
+monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+  target: monaco.languages.typescript.ScriptTarget.ESNext,
+  allowNonTsExtensions: true,
+  moduleResolution: monaco.languages.typescript.ModuleResolutionKind.NodeJs,
+  module: monaco.languages.typescript.ModuleKind.ESNext,
+  noEmit: true,
+  esModuleInterop: true,
+  jsx: monaco.languages.typescript.JsxEmit.ReactJSX,
+  reactNamespace: "React",
+  allowJs: true,
+  checkJs: true,
+  strict: true,
+})
 
 createRoot(document.getElementById("root")!).render(<App />)
