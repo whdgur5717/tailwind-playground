9479917e662c4c9625851769cc9634aa2294c693
whdgur5717 <whdgur57117@gmail.com>
2025-04-02 22:53:27 +0900

    typescript worker 모듈경로 해석 메서드 override

--- worker.js
+++ worker.js
@@ -1,5 +1,6 @@
 import { initialize, TypeScriptWorker } from "monaco-editor/esm/vs/language/typescript/ts.worker.js"
 import { URI as Uri } from "monaco-editor/esm/vs/base/common/uri.js"
+import ts from "typescript"
 
 class CustomTSWorker extends TypeScriptWorker {
   fileEntries = new Map()
@@ -53,6 +54,58 @@ class CustomTSWorker extends TypeScriptWorker {
   addFile(path, content) {
     console.log(`[CustomTSWorker] Adding file dynamically: ${path}`)
     this.fileEntries.set(path, content)
+  }
+
+  resolveModuleNames(
+    moduleNames,
+    containingFile,
+    reusedNames,
+    redirectedReference,
+    options,
+    containingSourceFile
+  ) {
+    const resolvedModules = []
+    const basePath = "inmemory://model/node_modules/" // URL 처리 시 필요할 수 있음
+
+    console.log(
+      `[CustomTSWorker] resolveModuleNames triggered for ${containingFile}. Resolving: ${moduleNames.join(
+        ", "
+      )}`
+    )
+
+    for (const moduleName of moduleNames) {
+      let resolvedModule = undefined
+      console.log(moduleName, "moduleName")
+
+      if (moduleName.includes("esm.sh")) {
+        //https://의 경로를 ts.resolveModuleName에서 처리못함 -> 바로 fileEntries에서 찾기
+        //extension의 경우 고정값이 아니긴 하지만, Dts로 고정해놓아도 오류는 없는것 같음
+        const exactPath = Array.from(this.fileEntries.keys()).filter(key =>
+          key.startsWith(basePath + moduleName)
+        )
+
+        resolvedModule = {
+          resolvedFileName: exactPath[0],
+          extension: ts.Extension.Dts,
+          isExternalLibraryImport: true,
+        }
+      } else {
+        const standardResult = ts.resolveModuleName(
+          moduleName,
+          containingFile,
+          options,
+          this // <- 중요: host로 'this'를 전달하여 해당 class의 메서드를 사용하게 할수 있는듯
+        )
+        console.log(standardResult)
+        if (standardResult.resolvedModule) {
+          resolvedModule = standardResult.resolvedModule
+        }
+      }
+
+      resolvedModules.push(resolvedModule) // 찾았으면 ResolvedModule 객체, 못 찾았으면 undefined 추가
+    }
+
+    return resolvedModules
   }
 }
 
