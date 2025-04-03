15143a1fdb41543fc4eb504751d30858693505d1
whdgur5717 <whdgur57117@gmail.com>
2025-04-03 18:47:49 +0900

    dedent, importMap 직접 적용 제거(srcDoc에서 인식못함)

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,17 +1,18 @@
-import { useRef, useState, useEffect } from "react"
+import { Button } from "@/components/ui/button"
 import * as monaco from "monaco-editor/esm/vs/editor/editor.api"
+import { useEffect, useRef, useState } from "react"
 import ts from "typescript"
-import dedent from "dedent"
 
 const defaultPackages = {
-  react: "https://esm.sh/react",
-  "react/": "https://esm.sh/react/",
-  "react-dom": "https://esm.sh/react-dom",
-  "react-dom/": "https://esm.sh/react-dom/",
-  "esbuild-wasm": "https://esm.sh/esbuild-wasm",
+	react: "https://esm.sh/react",
+	"react/": "https://esm.sh/react/",
+	"react-dom": "https://esm.sh/react-dom",
+	"react-dom/": "https://esm.sh/react-dom/",
+	"esbuild-wasm": "https://esm.sh/esbuild-wasm",
+	dedent: "https://esm.sh/dedent",
 } //기본적으로 사용될 패키지
 
-const defaultCode = dedent`
+const defaultCode = `
   import { createElement } from 'react';
   import { createRoot } from 'react-dom/client';
   function App() {
@@ -21,65 +22,66 @@ const defaultCode = dedent`
 `
 
 export const Editor = () => {
-  const [editor, setEditor] = useState<monaco.editor.IStandaloneCodeEditor | null>(null)
-  const monacoEl = useRef(null)
-  const [importMap, setImportMap] = useState(defaultPackages)
-  const [editorCode, setEditorCode] = useState(defaultCode)
+	const [editor, setEditor] =
+		useState<monaco.editor.IStandaloneCodeEditor | null>(null)
+	const monacoEl = useRef(null)
+	const [importMap, setImportMap] = useState(defaultPackages)
+	const [editorCode, setEditorCode] = useState(defaultCode)
 
-  const addPackage = async (moduleName: string, path: string) => {
-    setImportMap(prev => ({ ...prev, [moduleName]: path }))
-    const response = await fetch(path)
-    const typePath = response.headers.get("X-Typescript-Types")
-    if (!typePath) {
-      return
-    }
-    const importMap: Record<string, string> = {}
-    const processTypeFile = async (filePath: string) => {
-      if (importMap[filePath]) {
-        console.log(filePath)
-        return
-      }
-      const response = await fetch(filePath)
-      const text = await response.text()
-      importMap[filePath] = text
-      const refFiles = ts.preProcessFile(text, true, true)
-      if (!refFiles.importedFiles.length) {
-        return
-      }
+	const addPackage = async (moduleName: string, path: string) => {
+		setImportMap((prev) => ({ ...prev, [moduleName]: path }))
+		const response = await fetch(path)
+		const typePath = response.headers.get("X-Typescript-Types")
+		if (!typePath) {
+			return
+		}
+		const importMap: Record<string, string> = {}
+		const processTypeFile = async (filePath: string) => {
+			if (importMap[filePath]) {
+				console.log(filePath)
+				return
+			}
+			const response = await fetch(filePath)
+			const text = await response.text()
+			importMap[filePath] = text
+			const refFiles = ts.preProcessFile(text, true, true)
+			if (!refFiles.importedFiles.length) {
+				return
+			}
 
-      for (const file of refFiles.importedFiles) {
-        await processTypeFile(file.fileName)
-      }
-      return importMap
-    }
-    await processTypeFile(typePath)
+			for (const file of refFiles.importedFiles) {
+				await processTypeFile(file.fileName)
+			}
+			return importMap
+		}
+		await processTypeFile(typePath)
 
-    const worker = await monaco.languages.typescript.getTypeScriptWorker()
-    const currentWorker = await worker()
-    Object.entries(importMap).forEach(([key, value]) =>
-      currentWorker.addFile("inmemory://model/node_modules/" + key, value)
-    )
-  }
+		const worker = await monaco.languages.typescript.getTypeScriptWorker()
+		const currentWorker = await worker()
+		Object.entries(importMap).forEach(([key, value]) =>
+			currentWorker.addFile("inmemory://model/node_modules/" + key, value),
+		)
+	}
 
-  useEffect(() => {
-    if (monacoEl.current) {
-      setEditor(editor => {
-        if (editor) return editor
-        const ed = monaco.editor.create(monacoEl.current!, {
-          value: editorCode,
-          language: "typescript",
-          theme: "vs-dark",
-        })
-        ed.onDidChangeModelContent(() => {
-          setEditorCode(ed.getValue())
-        })
-        return ed
-      })
-    }
-    return () => editor?.dispose()
-  }, [monacoEl.current])
+	useEffect(() => {
+		if (monacoEl.current) {
+			setEditor((editor) => {
+				if (editor) return editor
+				const ed = monaco.editor.create(monacoEl.current, {
+					value: editorCode,
+					language: "typescript",
+					theme: "vs-dark",
+				})
+				ed.onDidChangeModelContent(() => {
+					setEditorCode(ed.getValue())
+				})
+				return ed
+			})
+		}
+		return () => editor?.dispose()
+	}, [monacoEl.current])
 
-  const previewScript = dedent`
+	const previewScript = `
     import * as esbuild from 'esbuild-wasm';
     await esbuild.default.initialize({
       worker: true,
@@ -97,12 +99,16 @@ export const Editor = () => {
     document.head.appendChild($script);
   `
 
-  const srcDoc = dedent`
+	const sources = {
+		imports: importMap,
+	}
+
+	const srcDoc = `
     <!DOCTYPE html>
     <html>
     <head>
       <script type="importmap">
-        ${JSON.stringify({ imports: importMap })}
+        ${JSON.stringify(sources)}
       </script>
       <script type="module">
         ${previewScript}
@@ -113,19 +119,25 @@ export const Editor = () => {
     </body>
     </html>
   `
-  return (
-    <div
-      style={{ display: "flex", gap: "10px", height: "100vh", width: "100vw", minWidth: "100vw" }}>
-      <div
-        ref={monacoEl}
-        style={{ flex: 1 }}></div>
-      <button onClick={() => addPackage("radix-ui", "https://esm.sh/radix-ui")}>
-        타입 파일 추가
-      </button>
-      <iframe
-        style={{ flex: 1, border: "none" }}
-        srcDoc={srcDoc}
-      />
-    </div>
-  )
+	return (
+		<div
+			style={{
+				display: "flex",
+				gap: "10px",
+				height: "100vh",
+				width: "100vw",
+				minWidth: "100vw",
+			}}
+		>
+			<div ref={monacoEl} style={{ flex: 1 }} />
+			<button onClick={() => addPackage("radix-ui", "https://esm.sh/radix-ui")}>
+				타입 파일 추가
+			</button>
+			<iframe
+				title="preview"
+				style={{ flex: 1, border: "none" }}
+				srcDoc={srcDoc}
+			/>
+		</div>
+	)
 }
