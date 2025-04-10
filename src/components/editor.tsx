7bd411b72f8c7d19a1c1d42c9f7384a3f47d064a
whdgur5717 <whdgur57117@gmail.com>
2025-04-10 18:38:25 +0900

    패키지 타입추가 관련 함수 제거

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,7 +1,6 @@
 import { observer } from "mobx-react-lite"
 import * as monaco from "monaco-editor/esm/vs/editor/editor.api"
 import { useEffect, useRef, useState } from "react"
-import ts from "typescript"
 import { editorStore } from "../store/editor"
 
 interface EditorProps {
@@ -18,49 +17,6 @@ const Editor = ({ className }: EditorProps) => {
 	const [models, setModels] = useState<Map<string, monaco.editor.ITextModel>>(
 		new Map(),
 	)
-
-	const addPackage = async (path: string) => {
-		const worker = await monaco.languages.typescript.getTypeScriptWorker()
-		const currentWorker = await worker()
-
-		const response = await fetch(path)
-		const typePath = response.headers.get("X-Typescript-Types")
-		if (!typePath) {
-			return
-		}
-
-		currentWorker.addUrl(path, typePath)
-
-		const importMap: Record<string, string> = {}
-
-		const processTypeFile = async (filePath: string) => {
-			if (importMap[filePath]) {
-				return
-			}
-			const response = await fetch(filePath)
-			const text = await response.text()
-			importMap[filePath] = text
-
-			const refFiles = ts.preProcessFile(text, true, true)
-			// 원본 코드와 동일하게 조건 유지
-			if (!refFiles.importedFiles.length && !refFiles.referencedFiles.length) {
-				return
-			}
-			for (const reference of refFiles.referencedFiles) {
-				const href = new URL(reference.fileName, filePath).href
-				await processTypeFile(href)
-			}
-			for (const file of refFiles.importedFiles) {
-				await processTypeFile(file.fileName)
-			}
-			return importMap
-		}
-		await processTypeFile(typePath)
-
-		Object.entries(importMap).forEach(([key, value]) =>
-			currentWorker.addFile("inmemory://model/node_modules/" + key, value),
-		)
-	}
 
 	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
 	useEffect(() => {
