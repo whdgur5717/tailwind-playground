1ba2b6da7c72dc2c0e5aa5dd0fbade4982f63c0a
whdgur5717 <whdgur57117@gmail.com>
2025-04-10 15:47:14 +0900

    페이지 렌더링 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import { EditorWrapper } from "./components/editorContext"
+import Editor from "./components/editor"
+import Preview from "./components/preview"
 import "./globals.css"
 function App() {
-	return <EditorWrapper />
+	return (
+		<div className="flex h-screen w-screen min-w-full flex-row gap-2.5 p-2.5">
+			<Editor className="min-w-0 flex-1" />
+			<Preview className='className="min-w-0 flex-1' />
+		</div>
+	)
 }
 
 export default App
